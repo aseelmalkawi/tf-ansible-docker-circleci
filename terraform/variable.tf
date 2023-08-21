55882eed30536590c33c36ef09d82f689c846ac0
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2023-08-21 14:23:14 +0300

    Delete variable.tf

--- terraform/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  type = string
-  default = "AKIA5CV2KHMXUIKOWFF4"
-}
-
-variable "aws_secret_key" {
-  type = string
-  default = "uqBtod7ziWOPgii1Ln22KINLFcVsut7qqy9KVwYs"
-}
