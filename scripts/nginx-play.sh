67c1c8e2731303a7fbfeacae36c069df64e4f976
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2023-08-21 10:39:58 +0300

    Update nginx-play.sh

--- scripts/nginx-play.sh
+++ scripts/nginx-play.sh
@@ -2,6 +2,9 @@ cat <<EOF > ../ansible/nginxPlaybook.yml
 - hosts: public
   become: true
   tasks:
+    - name: update
+      ansible.builtin.command: apt update
+
     - name: install nginx
       apt: 
         name: nginx 
@@ -19,15 +22,18 @@ cat <<EOF > ../ansible/nginxPlaybook.yml
     - name: Pause for a moment before restarting
       pause:
         seconds: 5
+
+    - name: update
+      ansible.builtin.command: apt update
     
     - name: Run nginx script
       ansible.builtin.script:
         cmd: ../scripts/nginx.sh $1 $2
     
+    - name: update
+      ansible.builtin.command: apt update
+    
     - name: Run nginx script
       ansible.builtin.script:
         cmd: ../scripts/nginxconfig.sh
-    
-    - name: update
-      ansible.builtin.command: apt update
 EOF
